d3874792d1f1f2a35fa98cf8005e759c355eef58
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-10-13 19:50:17 -0300

    feat: adding new informations about node

--- src/server.js
+++ src/server.js
@@ -1,9 +1,32 @@
-import http from 'node:http'
+import http from "node:http";
 
-const server = http.createServer((
-    
-) => {
+// GET => Buscar um recurso do back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+// DELETE => Deletar um recurso no back-end
 
-})
+const users = []
 
-server.listen(3333)
+const server = http.createServer((req, res) => {
+    const { method, url } = req
+
+    if (method === 'GET' && url === '/users') {
+        return res.end(users)
+    }
+
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@gmail.com'
+        })
+
+        return res.end('Criação de usuários')
+    }
+
+
+    return res.end("Hello World");
+});
+
+server.listen(3333);
