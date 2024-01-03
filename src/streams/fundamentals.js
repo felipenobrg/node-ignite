930aad552cbc0ef00a71cdbeb62a680f43a90cf6
Felipe Nóbrega <felipenobrega2012@gmail.com>
2024-01-03 20:04:19 -0300

    feat: creating transform and writeable stream

--- src/streams/fundamentals.js
+++ src/streams/fundamentals.js
@@ -3,7 +3,7 @@
 // process.stdin
 //    .pipe(process.stdout)
 
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
 class OneToHundredStream extends Readable {
    index = 1
@@ -19,9 +19,24 @@ class OneToHundredStream extends Readable {
       const buf = Buffer.from(String(i))
       this.push(buf)
     }
-   }, 1000)
+   }, 0.1)
 }
 }
 
+class InverseNumberStream extends Transform {
+   _transform(chunk, encoding, callback) {
+      const transformed = Number(chunk.toString()) * -1
+      callback(null, Buffer.from(String(transformed)))
+   }
+}
+
+class MultiplyByTenStream extends Writable {
+   _write(chunk, encoding, callback) {
+      console.log(chunk.toString() * 10)
+      callback()
+   }
+}
+
 new OneToHundredStream()
-   .pipe(process.stdout)
+   .pipe(new InverseNumberStream())
+   .pipe(new MultiplyByTenStream())
