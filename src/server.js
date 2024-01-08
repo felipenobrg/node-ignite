68cef81b36f44c4ba317dd5376eb86a28fa331e8
Felipe Nóbrega <felipenobrega2012@gmail.com>
2024-01-08 19:23:38 -0300

    feat:  removing registers

--- src/server.js
+++ src/server.js
@@ -1,41 +1,22 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
-import { Database } from "./database.js";
-import { randomUUID } from "node:crypto";
-
-// GET => Buscar um recurso do back-end
-// POST => Criar um recurso no back-end
-// PUT => Atualizar um recurso no back-end
-// PATCH => Atualizar uma informação específica de um recurso no back-end
-// DELETE => Deletar um recurso no back-end
-
-// Cabeçalhos (Requisicao Resposta) => Metadados
-
-const database = new Database();
+import { routes } from "./routes.js";
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res);
 
-  if (method === "GET" && url === "/users") {
-    const users = database.select("users");
+  const route = routes.find(route => {
+    return route.method === method && route.path.test(url)
+  })
 
-    return res.end(JSON.stringify(users));
-  }
+  if (route) {
+    const routeParams = req.url.match(route.path)
 
-  if (method === "POST" && url === "/users") {
-    const { name, email } = req.body;
+    req.params = { ...routeParams.groups}
 
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    };
-
-    database.insert("users", user);
-
-    return res.writeHead(201).end();
+    route.handler(req, res)
   }
 
   return res.writeHead(404).end();
